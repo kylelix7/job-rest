2f35df984ec3e8e0730f0ee2fc2adf412b6f179a
Kyle Li <kyle.lix7@gmail.com>
2017-04-16 23:07:50 -0400

    plot scotia, need refactor

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var JobSchema = new Schema({name: String});
+
+mongoose.connect('mogodb://localhost:27017');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
