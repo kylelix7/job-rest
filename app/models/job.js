2696270b667468f63db797f4d0286ba7a16cbb49
Kyle Li <kyle.lix7@gmail.com>
2017-04-22 23:21:52 -0400

    job model

---
+++ app/models/job.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var KeywordSchema = new Schema({keyword: String, count: String});
+var JobSchema = new Schema({
+	id: String,
+  company: String,
+  title: String,
+  post_date: String,
+  city: String,
+  province: String,
+  content: String,
+  link: String,
+  stats: [KeywordSchema]
+});
+
+module.exports = mongoose.model('Job', JobSchema);
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var JobSchema = new Schema({name: String});
+var Job = require('./app/models/job');
 
 mongoose.connect('mogodb://localhost:27017');
 
@@ -20,6 +20,21 @@ var router = express.Router();
 router.get('/', function(req, res){
     res.json({message:'done!'});
 });
+
+
+// GET jobs
+router.route('/jobs')
+  .get(function(req, res) {
+    Job.find(function(err, jobs) {
+      if(err) {
+        res.send(err);
+      } else {
+        res.json(jobs);
+      }
+    });
+  });
+// GET banks
+
 
 app.use('/api', router);
 
