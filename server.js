1a8358c9812617a4b90b55914b2a263be90480cd
Kyle Li <kyle.lix7@gmail.com>
2017-05-19 22:39:38 -0400

    count

--- server.js
+++ server.js
@@ -73,19 +73,21 @@ router.route('/jobs')
 //   [{$group:
 //       {
 //         _id: null,
-//         total_java: { $sum: "$stats.Java"},
-//         total_Agile: {$sum: "$stats.Agile"}
+//         java: { $sum: "$stats.Java"},
+//         Agile: {$sum: "$stats.Agile"}
 //       }
 //    } ])
 router.route('/stats')
   .get(function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    var body = req.body;
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    var bank = req.query.company;
     var match = {};
-    if (body && body.company) {
-      match = {"company": body.company};
+    if (bank) {
+      match = {"company": bank};
     }
+
     Job.aggregate([
       {
         "$match": match
@@ -93,135 +95,134 @@ router.route('/stats')
       {
         "$group": {
           "_id": null,
-          "total_java": {$sum: "$stats.Java"},
-          "total_agile": {$sum: "$stats.Agile"},
-          "total_Linux/Unix": {$sum: "$stats.Linux/Unix"},
-          "total_AIX": {$sum: "$stats.AIX"},
-          "total_Windows": {$sum: "$stats.Windows"},
-          "total_Tomcat": {$sum: "$stats.Tomcat"},
-          "total_Websphere": {$sum: "$stats.Websphere"},
-          "total_Weblogic": {$sum: "$stats.Weblogic"},
-          "total_Nginx": {$sum: "$stats.Nginx"},
-          "total_Jboss": {$sum: "$stats.Jboss"},
-          "total_IIS": {$sum: "$stats.IIS"},
-          "total_Zookeeper": {$sum: "$stats.Zookeeper"},
-          "total_Kafka": {$sum: "$stats.Kafka"},
-          "total_DB/Database/Databases/DBMS": {$sum: "$stats.DB/Database/Databases/DBMS"},
-          "total_MySQL": {$sum: "$stats.MySQL"},
-          "total_DB2": {$sum: "$stats.DB2"},
-          "total_Oracle": {$sum: "$stats.Oracle"},
-          "total_Netezza": {$sum: "$stats.Netezza"},
-          "total_Postgres": {$sum: "$stats.Postgres"},
-          "total_Redis": {$sum: "$stats.Redis"},
-          "total_Memchached": {$sum: "$stats.Memcached"},
-          "total_Hive": {$sum: "$stats.Hive"},
-          "total_Cassandra": {$sum: "$stats.Cassandra"},
-          "total_DynamoDB": {$sum: "$stats.DynamoDB"},
-          "total_SimpleDB": {$sum: "$stats.SimpleDB"},
-          "total_Elasticsearch": {$sum: "$stats.Elasticsearch"},
-          "total_Firebase": {$sum: "$stats.Firebase"},
-          "total_FoundationDB": {$sum: "$stats.FoundationDB"},
-          "total_Lucene": {$sum: "$stats.Lucene"},
-          "total_MariaDB": {$sum: "$stats.MariaDB"},
-          "total_Neo4j": {$sum: "$stats.Neo4j"},
-          "total_SQLite": {$sum: "$stats.SQLite"},
-          "total_SQLserver/SQL server": {$sum: "$stats.SQLserver/SQL server"},
-          "total_NoSQL": {$sum: "$stats.NoSQL"},
-          "total_MongoDB": {$sum: "$stats.MongoDB"},
-          "total_CouchDB": {$sum: "$stats.CouchDB"},
-          "total_Perl": {$sum: "$stats.Perl"},
-          "total_Bash/Shell": {$sum: "$stats.Bash/Shell"},
-          "total_Erlang": {$sum: "$stats.Erlang"},
-          "total_Golang/Google Go": {$sum: "$stats.Golang/Google Go"},
-          "total_PHP": {$sum: "$stats.PHP"},
-          "total_Smalltalk": {$sum: "$stats.Smalltalk"},
-          "total_Java": {$sum: "$stats.Java"},
-          "total_JaveEE/Jave EE/J2EE": {$sum: "$stats.JavaEE/Java EE/J2EE"},
-          "total_Spring": {$sum: "$stats.Spring"},
-          "total_Structs": {$sum: "$stats.Structs"},
-          "total_Hibernate": {$sum: "$stats.Hibernate"},
-          "total_Maven": {$sum: "$stats.Maven"},
-          "total_Gradle": {$sum: "$stats.Gradle"},
-          "total_JDBC": {$sum: "$stats.JDBC"},
-          "total_Solr": {$sum: "$stats.Solr"},
-          "total_Android": {$sum: "$stats.Android"},
-          "total_iOS": {$sum: "$stats.iOS"},
-          "total_Net": {$sum: "$stats.-Net/ASP-NET"},
-          "total_C#": {$sum: "$stats.C#"},
-          "total_Xamarin": {$sum: "$stats.Xarmarin"},
-          "total_C": {$sum: "$stats.C"},
-          "total_C++": {$sum: "$stats.C++"},
-          "total_nodejs": {$sum: "$stats.Node-js/nodejs"},
-          "total_Javascript/Java script/JS": {$sum: "$stats.Javascript/Java script/JS"},
-          "total_Angular": {$sum: "$stats.Angular"},
-          "total_react": {$sum: "$stats.react"},
-          "total_Expressjs": {$sum: "$stats.Express-js/Expressjs"},
-          "total_Bootstrap": {$sum: "$stats.Bootstrap"},
-          "total_Python": {$sum: "$stats.Python"},
-          "total_TensorFlow": {$sum: "$stats.TensorFlow"},
-          "total_Scikit-learn/Scikit learn": {$sum: "$stats.Scikit-learn/Scikit learn"},
-          "total_Django": {$sum: "$stats.Django"},
-          "total_Machine learning": {$sum: "$stats.Machine learning"},
-          "total_Jenkins": {$sum: "$stats.Jenkins"},
-          "total_Travis": {$sum: "$stats.Travis/travis-ci"},
-          "total_Selenium": {$sum: "$stats.Selenium"},
-          "total_UrbanCode Deploy/UCD": {$sum: "$stats.UrbanCode Deploy/UCD"},
-          "total_Chef": {$sum: "$stats.Chef"},
-          "total_Ansible": {$sum: "$stats.Ansible"},
-          "total_Cloud": {$sum: "$stats.Cloud"},
-          "total_REST/Restful": {$sum: "$stats.REST/Restful"},
-          "total_SOAP": {$sum: "$stats.SOAP"},
-          "total_API/web service/webservice": {$sum: "$stats.API/web service/webservice"},
-          "total_Docker": {$sum: "$stats.Docker"},
-          "total_Container": {$sum: "$stats.Container"},
-          "total_Kubernetes/k8s": {$sum: "$stats.Kubernetes/k8s"},
-          "total_Docker swarm": {$sum: "$stats.Docker swarm"},
-          "total_AWS": {$sum: "$stats.AWS"},
-          "total_Azure": {$sum: "$stats.Azure"},
-          "total_Bluemix": {$sum: "$stats.Bluemix"},
-          "total_Hadoop": {$sum: "$stats.Hadoop"},
-          "total_HDFS": {$sum: "$stats.HDFS"},
-          "total_HBase": {$sum: "$stats.HBase"},
-          "total_Spark": {$sum: "$stats.Spark"},
-          "total_MapReduce": {$sum: "$stats.MapReduce"},
-          "total_YARN": {$sum: "$stats.YARN"},
-          "total_Storm": {$sum: "$stats.Storm"},
-          "total_Flink": {$sum: "$stats.Flink"},
-          "total_Samza": {$sum: "$stats.Samza"},
-          "total_Pig": {$sum: "$stats.Pig"},
-          "total_Sqoop": {$sum: "$stats.Sqoop"},
-          "total_Flume": {$sum: "$stats.Flume"},
-          "total_LDAP": {$sum: "$stats.LDAP"},
-          "total_Active Directory": {$sum: "$stats.Active Directory"},
-          "total_Kerberos": {$sum: "$stats.Kerberos"},
-          "total_PAM": {$sum: "$stats.PAM"},
-          "total_HTML/HTML5": {$sum: "$stats.HTML/HTML5"},
-          "total_CSS": {$sum: "$stats.CSS"},
-          "total_XML": {$sum: "$stats.XML"},
-          "total_JSON": {$sum: "$stats.JSON"},
-          "total_TypeScript": {$sum: "$stats.TypeScript"},
-          "total_CoffeScript": {$sum: "$stats.CoffeeScript"},
-          "total_Dart": {$sum: "$stats.Dart"},
-          "total_Rust": {$sum: "$stats.Rust"},
-          "total_Ruby": {$sum: "$stats.Ruby"},
-          "total_Ruby on Rails/RoR": {$sum: "$stats.Ruby on Rails/RoR"},
-          "total_Clojure": {$sum: "$stats.Clojure"},
-          "total_Groovy": {$sum: "$stats.Groovy"},
-          "total_Jade": {$sum: "$stats.Jade"},
-          "total_Lua": {$sum: "$stats.Lua"},
-          "total_Objective-C": {$sum: "$stats.Objective-C/Objective C/ObjectiveC"},
-          "total_Swift": {$sum: "$stats.Swift"},
-          "total_Git": {$sum: "$stats.Git"},
-          "total_Jira": {$sum: "$stats.Jira"},
-          "total_SVN": {$sum: "$stats.SVN"},
-          "total_Karma": {$sum: "$stats.Karma"},
-          "total_Protractor": {$sum: "$stats.Protractor"},
-          "total_Jasmine": {$sum: "$stats.Jasmine"},
-          "total_Gulp": {$sum: "$stats.Gulp"},
-          "total_Grunt": {$sum: "$stats.Grunt"},
-          "total_Webpack": {$sum: "$stats.Webpack"},
-          "total_DevOps": {$sum: "$stats.DevOps"},
-          "total_COBOL": {$sum: "$stats.COBOL"}
+          "Agile": {$sum: "$stats.Agile"},
+          "Linux/Unix": {$sum: "$stats.Linux/Unix"},
+          "AIX": {$sum: "$stats.AIX"},
+          "Windows": {$sum: "$stats.Windows"},
+          "Tomcat": {$sum: "$stats.Tomcat"},
+          "Websphere": {$sum: "$stats.Websphere"},
+          "Weblogic": {$sum: "$stats.Weblogic"},
+          "Nginx": {$sum: "$stats.Nginx"},
+          "Jboss": {$sum: "$stats.Jboss"},
+          "IIS": {$sum: "$stats.IIS"},
+          "Zookeeper": {$sum: "$stats.Zookeeper"},
+          "Kafka": {$sum: "$stats.Kafka"},
+          "DB/Database/Databases/DBMS": {$sum: "$stats.DB/Database/Databases/DBMS"},
+          "MySQL": {$sum: "$stats.MySQL"},
+          "DB2": {$sum: "$stats.DB2"},
+          "Oracle": {$sum: "$stats.Oracle"},
+          "Netezza": {$sum: "$stats.Netezza"},
+          "Postgres": {$sum: "$stats.Postgres"},
+          "Redis": {$sum: "$stats.Redis"},
+          "Memchached": {$sum: "$stats.Memcached"},
+          "Hive": {$sum: "$stats.Hive"},
+          "Cassandra": {$sum: "$stats.Cassandra"},
+          "DynamoDB": {$sum: "$stats.DynamoDB"},
+          "SimpleDB": {$sum: "$stats.SimpleDB"},
+          "Elasticsearch": {$sum: "$stats.Elasticsearch"},
+          "Firebase": {$sum: "$stats.Firebase"},
+          "FoundationDB": {$sum: "$stats.FoundationDB"},
+          "Lucene": {$sum: "$stats.Lucene"},
+          "MariaDB": {$sum: "$stats.MariaDB"},
+          "Neo4j": {$sum: "$stats.Neo4j"},
+          "SQLite": {$sum: "$stats.SQLite"},
+          "SQLserver/SQL server": {$sum: "$stats.SQLserver/SQL server"},
+          "NoSQL": {$sum: "$stats.NoSQL"},
+          "MongoDB": {$sum: "$stats.MongoDB"},
+          "CouchDB": {$sum: "$stats.CouchDB"},
+          "Perl": {$sum: "$stats.Perl"},
+          "Bash/Shell": {$sum: "$stats.Bash/Shell"},
+          "Erlang": {$sum: "$stats.Erlang"},
+          "Golang/Google Go": {$sum: "$stats.Golang/Google Go"},
+          "PHP": {$sum: "$stats.PHP"},
+          "Smalltalk": {$sum: "$stats.Smalltalk"},
+          "Java": {$sum: "$stats.Java"},
+          "JaveEE/Jave EE/J2EE": {$sum: "$stats.JavaEE/Java EE/J2EE"},
+          "Spring": {$sum: "$stats.Spring"},
+          "Structs": {$sum: "$stats.Structs"},
+          "Hibernate": {$sum: "$stats.Hibernate"},
+          "Maven": {$sum: "$stats.Maven"},
+          "Gradle": {$sum: "$stats.Gradle"},
+          "JDBC": {$sum: "$stats.JDBC"},
+          "Solr": {$sum: "$stats.Solr"},
+          "Android": {$sum: "$stats.Android"},
+          "iOS": {$sum: "$stats.iOS"},
+          "Net": {$sum: "$stats.-Net/ASP-NET"},
+          "C#": {$sum: "$stats.C#"},
+          "Xamarin": {$sum: "$stats.Xarmarin"},
+          "C": {$sum: "$stats.C"},
+          "C++": {$sum: "$stats.C++"},
+          "nodejs": {$sum: "$stats.Node-js/nodejs"},
+          "Javascript/Java script/JS": {$sum: "$stats.Javascript/Java script/JS"},
+          "Angular": {$sum: "$stats.Angular"},
+          "react": {$sum: "$stats.react"},
+          "Expressjs": {$sum: "$stats.Express-js/Expressjs"},
+          "Bootstrap": {$sum: "$stats.Bootstrap"},
+          "Python": {$sum: "$stats.Python"},
+          "TensorFlow": {$sum: "$stats.TensorFlow"},
+          "Scikit-learn/Scikit learn": {$sum: "$stats.Scikit-learn/Scikit learn"},
+          "Django": {$sum: "$stats.Django"},
+          "Machine learning": {$sum: "$stats.Machine learning"},
+          "Jenkins": {$sum: "$stats.Jenkins"},
+          "Travis": {$sum: "$stats.Travis/travis-ci"},
+          "Selenium": {$sum: "$stats.Selenium"},
+          "UrbanCode Deploy/UCD": {$sum: "$stats.UrbanCode Deploy/UCD"},
+          "Chef": {$sum: "$stats.Chef"},
+          "Ansible": {$sum: "$stats.Ansible"},
+          "Cloud": {$sum: "$stats.Cloud"},
+          "REST/Restful": {$sum: "$stats.REST/Restful"},
+          "SOAP": {$sum: "$stats.SOAP"},
+          "API/web service/webservice": {$sum: "$stats.API/web service/webservice"},
+          "Docker": {$sum: "$stats.Docker"},
+          "Container": {$sum: "$stats.Container"},
+          "Kubernetes/k8s": {$sum: "$stats.Kubernetes/k8s"},
+          "Docker swarm": {$sum: "$stats.Docker swarm"},
+          "AWS": {$sum: "$stats.AWS"},
+          "Azure": {$sum: "$stats.Azure"},
+          "Bluemix": {$sum: "$stats.Bluemix"},
+          "Hadoop": {$sum: "$stats.Hadoop"},
+          "HDFS": {$sum: "$stats.HDFS"},
+          "HBase": {$sum: "$stats.HBase"},
+          "Spark": {$sum: "$stats.Spark"},
+          "MapReduce": {$sum: "$stats.MapReduce"},
+          "YARN": {$sum: "$stats.YARN"},
+          "Storm": {$sum: "$stats.Storm"},
+          "Flink": {$sum: "$stats.Flink"},
+          "Samza": {$sum: "$stats.Samza"},
+          "Pig": {$sum: "$stats.Pig"},
+          "Sqoop": {$sum: "$stats.Sqoop"},
+          "Flume": {$sum: "$stats.Flume"},
+          "LDAP": {$sum: "$stats.LDAP"},
+          "Active Directory": {$sum: "$stats.Active Directory"},
+          "Kerberos": {$sum: "$stats.Kerberos"},
+          "PAM": {$sum: "$stats.PAM"},
+          "HTML/HTML5": {$sum: "$stats.HTML/HTML5"},
+          "CSS": {$sum: "$stats.CSS"},
+          "XML": {$sum: "$stats.XML"},
+          "JSON": {$sum: "$stats.JSON"},
+          "TypeScript": {$sum: "$stats.TypeScript"},
+          "CoffeScript": {$sum: "$stats.CoffeeScript"},
+          "Dart": {$sum: "$stats.Dart"},
+          "Rust": {$sum: "$stats.Rust"},
+          "Ruby": {$sum: "$stats.Ruby"},
+          "Ruby on Rails/RoR": {$sum: "$stats.Ruby on Rails/RoR"},
+          "Clojure": {$sum: "$stats.Clojure"},
+          "Groovy": {$sum: "$stats.Groovy"},
+          "Jade": {$sum: "$stats.Jade"},
+          "Lua": {$sum: "$stats.Lua"},
+          "Objective-C": {$sum: "$stats.Objective-C/Objective C/ObjectiveC"},
+          "Swift": {$sum: "$stats.Swift"},
+          "Git": {$sum: "$stats.Git"},
+          "Jira": {$sum: "$stats.Jira"},
+          "SVN": {$sum: "$stats.SVN"},
+          "Karma": {$sum: "$stats.Karma"},
+          "Protractor": {$sum: "$stats.Protractor"},
+          "Jasmine": {$sum: "$stats.Jasmine"},
+          "Gulp": {$sum: "$stats.Gulp"},
+          "Grunt": {$sum: "$stats.Grunt"},
+          "Webpack": {$sum: "$stats.Webpack"},
+          "DevOps": {$sum: "$stats.DevOps"},
+          "COBOL": {$sum: "$stats.COBOL"}
         }
       }
     ], function (err, result) {
@@ -229,13 +230,11 @@ router.route('/stats')
         res.send(err);
       } else {
         var arr = [];
-        if(Array.isArray(result) && result.length > 0) {
+        if (Array.isArray(result) && result.length > 0) {
           result = result[0];
           delete result['_id'];
           for (var key in result) {
             if (result.hasOwnProperty(key)) {
-              console.log('=====');
-              console.log(key);
               arr.push({name: key, value: result[key]});
             }
           }
@@ -248,6 +247,50 @@ router.route('/stats')
     });
   });
 
+// TODO GET count
+router.route('/count')
+  .get(function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    query_object = {};
+    var bank_name = req.query.bank;
+    if (bank_name) {
+      query_object.company = bank_name;
+    }
+    var skill = req.query.skill;
+    if (skill) {
+      skill = skill.replace(/\./g, '-');
+      query_object['stats.' + skill] = {$gt: 0};
+    }
+    var page = req.query.page;
+    if (page) {
+      page = parseInt(page);
+    } else {
+      page = 0;
+    }
+    var page_size = req.query.page_size;
+    if (page_size) {
+      page_size = parseInt(page_size);
+    } else {
+      page_size = 20;
+    }
+    var skip = page_size * page;
+
+    Job.find(query_object)
+      .sort('-post_date')
+      .limit(page_size)
+      .skip(skip)
+      .exec(function (err, jobs) {
+        if (err) {
+          res.send(err);
+        } else {
+          Job.count(query_object, function (err, c) {
+            result = {count: c, jobs: jobs};
+            res.json(result);
+          });
+        }
+      });
+  });
 app.use('/api', router);
 
 app.listen(port);
